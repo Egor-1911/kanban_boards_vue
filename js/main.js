5ebaefde5637ec7e8994ab7c387c0e77c39f01d9
gr434_mueva <egormyrt21@gmail.com>
2026-02-09 16:26:34 +0700

    commit form for adding the task

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 let app = new Vue({
     el: '#app',
     data: {
+        newTask: {
+            title: '',
+            description: '',
+            deadline: ''
+        },
         columns: [
             {
                 id: 1,
@@ -23,6 +28,28 @@ let app = new Vue({
                 tasks: []
             },
         ]
+    },
+    methods: {
+        addTask() {
+            if (this.newTask.title.trim()) {
+                const task = {
+                    id: Date.now(),
+                    title: this.newTask.title,
+                    description: this.newTask.description,
+                    createdAt: new Date().toISOString(),
+                    deadline: this.newTask.deadline,
+                    updatedAt: new Date().toISOString()
+                };
+
+                this.columns[0].tasks.push(task);
+
+                this.newTask = {
+                    title: '',
+                    description: '',
+                    deadline: ''
+                };
+            }
+        }
     }
 
 })
